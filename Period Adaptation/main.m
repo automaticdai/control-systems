79ef4f184bc3d4fded4b55f360753c0212ba9931
automaticdai <automatic.dai@gmail.com>
2017-09-27 22:27:30 +0100

    changed period adaptation code

--- Period Adaptation/main.m
+++ Period Adaptation/main.m
@@ -1,38 +1,92 @@
+% Objective: minimize h while make the system stable.
+% Xiaotian Dai
+
 syms s
 s = tf('s');
 
-% system model (simple first order system)
-% time constant: 4
+% reference model (simple first-order system)
+% time constant: 4s, steady-state: 12s
+% for first-order system
 % response vs tau: 1 tau-63.2, 2 tau-86.5, 3 tau-95.0
-Gs = 3 / (4*s + 1);
-Cs = (0.1*s + 1) / (0.1*s);
+Gs_actual = 3 / (4*s + 1);
 
-Gsc = feedback(Gs * Cs, 1);
+% actual system model
+% time constant: 6s, steady-state time: 6*3 = 18s
+Gs = 3 / (6*s + 1);
+Kp = 2.8;
+Ti = 1.1; 
+Cs = Kp * (Ti*s + 1) / (Ti*s);
+Hsc = feedback(Gs * Cs, 1);
 
-h = 0.01;
 
+
+for i = 0:0
+    
+h = 0.12 + 0.012 * i;
+
+% evaluate stability
+Gsd = c2d(Gs, h);
+Csd = c2d(Cs, h);
+Hscd = (Gsd*Csd)/(1 + Gsd*Csd);
+
+
+% run simlink
 sim('discreate_control_by_period')
+u = input.data;
+y = output.data;
+t = output.time;
 
-% estimate the transfer function
-tfdata = iddata(input.data, output.data, h);
-N = 1;                  % Number of poles
-sys = tfest(tfdata,N);  
+% performance index
+ss_region_logic = ~(y > 0.95 & y < 1.05);
+ss_region_idx = find(ss_region_logic, 1, 'last');
+
+pi_ss_time = t(ss_region_idx);
+pi_state_cost = sum((1-y).^2 * h);
+pi_control_cost = sum(u .^ 2 * h);
+pi_quad_cost = pi_state_cost + pi_control_cost;
+%%pi_stability = ;
+
+
+%% plot PIs
+subplot(4,1,1)
+scatter(h, pi_ss_time, 'b');
+hold on;
+
+subplot(4,1,2)
+scatter(h, pi_state_cost, 'r');
+hold on;
+
+subplot(4,1,3)
+scatter(h, pi_control_cost, 'g');
+hold on;
+
+subplot(4,1,4)
+scatter(h, pi_quad_cost, 'y');
+hold on;
+
+end
 
 
 
 
-
-
-
-% [y,t] = step(Gsc);
-% plot(t,y)
+%% estimate the transfer function
+% tfdata = iddata(y, u, h);
+% N = 1;                  % Number of poles
+% sys = tfest(tfdata,N);
+% 
+% Esimate a parametric model.
+% sys1 = ssest(z1,4);
+% 
+% Estimate a non-parametric model.
+% sys2 = impulseest(z1);
+% 
+% [y1, ~, ~, ysd1] = step(sys, t);
+% plot(t, y1, 'b', t, y1+3*ysd1, 'b:', t, y1-3*ysd1, 'b:')
 % hold on;
+% 
+% 
+% [y2, ~] = step(Gs, t);
+% plot(t, y2, 'r')
 
-% for i = 1:1
-%     % closed-loop discrete function
-%     Gs_d = c2d(Gsc, 0.5 + 1e-2 * i);
-%     step(Gs_d);
-%     %plot(t,y)
-%     hold on;
-%     pause(0.1)
-% end
+
+
